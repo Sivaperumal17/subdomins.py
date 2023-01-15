caebef48263361690ef644bc173fe0727ddd804b
Sivaperumal17 <118950365+Sivaperumal17@users.noreply.github.com>
2023-01-15 05:33:08 -0500

    Add files via upload

---
+++ subdomins.py
@@ -0,0 +1,11 @@
+  #!/usr/bin/python
+import requests
+def requests(url):
+         try:
+              return requests.get("http://"url)
+         except 
+               return .exceptions.ConnectionError:
+                pass
+
+
+target_url=raw
